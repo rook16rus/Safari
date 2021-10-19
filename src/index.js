c6bab4ca218623b501d6605368dd669530fcceb5
rook16rus <kamalov00albert@gmail.com>
2021-10-19 09:35:40 +0300

    Начальная сборка проекта

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './sass/reset.css';
+import './sass/slick.css';
+import $ from 'jquery';
+import './sass/jquery.fancybox.css'
+import './js/jquery.fancybox.min'
+import './js/slick.min'
+
+
+$(function(){
+
+});
