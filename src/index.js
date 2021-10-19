b2ffdfbc11f142e525aee26f9f9ee89b0901c3d0
rook16rus <kamalov00albert@gmail.com>
2021-10-19 16:19:34 +0300

    Стилизовал блок о сафари турах. Реализовал попап для видео

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import './sass/reset.css';
 import './sass/slick.css';
 import './sass/jquery.fancybox.css'
+import './sass/magnific-popup.css'
 import './sass/style.scss'
 import $ from 'jquery';
 import './js/jquery.fancybox.min'
 import './js/slick.min'
+import './js/jquery.magnific-popup.min'
 
 
 $(function(){
+    $('.popup__link').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
 
+        fixedContentPos: false
+    });
 });
