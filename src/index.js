27c88616dce4020d2e0bcd97051a46893aeed953
rook16rus <kamalov00albert@gmail.com>
2021-10-20 12:00:18 +0300

    Стилизовал блок с галлереей

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import $ from 'jquery';
 import './js/jquery.fancybox.min'
 import './js/slick.min'
 import './js/jquery.magnific-popup.min'
+import './images/gallery-arrow-left.svg'
+import './images/gallery-arrow-right.svg'
 
 
 $(function(){
@@ -18,5 +20,23 @@ $(function(){
         preloader: false,
 
         fixedContentPos: false
+    });
+
+    $('.gallery__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-btn--prev"><img src="./gallery-arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-btn--next"><img src="./gallery-arrow-right.svg" alt=""></button>',
+
+    });
+
+    $('.gallery__item-inner').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
     });
 });
